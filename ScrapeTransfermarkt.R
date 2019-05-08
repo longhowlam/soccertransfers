5acfe6c9576e3973db27890015ead3bb94e58bb3
longhowlam <longhowlam@gmail.com>
2019-05-08 12:24:22 +0100

    mba

--- ScrapeTransfermarkt.R
+++ ScrapeTransfermarkt.R
@@ -1,12 +1,20 @@
+#### Libraries needed #############################################################################
 library(rvest)
 library(dplyr)
 library(igraph)
 library(visNetwork)
-
 library(wordcloud)
+library(stringr)
+library(forcats)
+library(ggplot2)
+library(mlr)
+
+
+#### Scrape Dutch transfers ######################################################################
 
 AllTransfers = data.frame()
-for( jaar in 1993:2018 ){
+
+for( jaar in 1992:2018 ){
   print(jaar)
   link = paste0(
     "https://www.transfermarkt.nl/eredivisie/transfers/wettbewerb/NL1/plus/?saison_id=",
@@ -40,19 +48,72 @@ for( jaar in 1993:2018 ){
   AllTransfers = bind_rows(AllTransfers, result)
 }
 
+## remove some duplicated records en clubnames
+AllTransfers$NieuweClub = ifelse( AllTransfers$`Nieuwe club` == "SBV Excelsior", "Excelsior", AllTransfers$`Nieuwe club`)
+AllTransfers$VorigeClub = ifelse( AllTransfers$`Vorige club` == "SBV Excelsior", "Excelsior", AllTransfers$`Vorige club`)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "AFC Ajax", "Ajax", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "AFC Ajax", "Ajax", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "Vitesse Arnhem", "Vitesse", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "Vitesse Arnhem", "Vitesse", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "RBC Roosendaal", "RBC", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "RBC Roosendaal", "RBC", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "Sparta Rotterdam", "Sparta", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "Sparta Rotterdam", "Sparta", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "NEC Nijmegen", "NEC", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "NEC Nijmegen", "NEC", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "Heracles Almelo", "Heracles", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "Heracles Almelo", "Heracles", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "Cambuur L.", "SC Cambuur", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "Cambuur L.", "SC Cambuur", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "MVV Maastricht", "MVV", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "MVV Maastricht", "MVV", AllTransfers$VorigeClub)
+AllTransfers$NieuweClub = ifelse( AllTransfers$NieuweClub == "sc Heerenveen", "Heerenveen", AllTransfers$NieuweClub)
+AllTransfers$VorigeClub = ifelse( AllTransfers$VorigeClub == "sc Heerenveen", "Heerenveen", AllTransfers$VorigeClub)
 
-AllTransfers = AllTransfers %>%  select(-Aanwinst, -`Vertrokken speler`, -Nat.)
-AllTransfers$`Vertrokken speler` = NULL
 
-tmp = AllTransfers %>% group_by(`Nieuwe club`) %>%  summarise(n=n())
-tmp2 = AllTransfers %>% group_by(`Vorige club`) %>%  summarise(n=n())
+## Remove columns we do not need any more and remove duplicates
+AllTransfers2 = AllTransfers %>%  
+  filter(
+    VorigeClub != "Onbekend" ,
+    NieuweClub != "Onbekend" 
+  ) %>% 
+  select(-Aanwinst, -`Vertrokken speler`, -Nat., `Nieuwe club`, `Vorige club`) %>% 
+  distinct()
 
-##############################################################################
+## there is a long tail of small clubs, rename them to other
+AllTransfers2 = AllTransfers2 %>% mutate(NieuweClub = fct_lump(NieuweClub, n=70) %>% as.character)
+AllTransfers2 = AllTransfers2 %>% mutate(VorigeClub = fct_lump(VorigeClub, n=70) %>% as.character)
 
-edges = AllTransfers %>% rename(from = `Vorige club`, to = `Nieuwe club` ) %>% filter(jaar > 1015) %>%  select(from,to) 
+club1 = AllTransfers2 %>% group_by(NieuweClub) %>%  summarise(n=n())
+club2 = AllTransfers2 %>% group_by(VorigeClub) %>%  summarise(n=n())
+positie = AllTransfers2 %>% group_by(Positie) %>% summarise(n=n(), leeftijd = mean(Leeftijd))
+
+### some plots
+AllTransfers3 = AllTransfers2 %>% 
+  filter(Leeftijd > 17) %>% 
+  mutate(
+    keeper = ifelse(Positie =="Keeper",1,0),
+    Centrumspits = ifelse(Positie =="Centrumspits",1,0),
+    Centraleverdediger = ifelse(Positie =="Centrale verdediger",1,0)
+  )
+
+ggplot(AllTransfers3, aes(Leeftijd, keeper)) + 
+  geom_smooth(se=FALSE) +
+  geom_smooth( aes(Leeftijd, Centrumspits), se=FALSE) +
+  geom_smooth( aes(Leeftijd, Centraleverdediger), se=FALSE)
+
+##### Create an igraph object #####################################################################
+
+edges = AllTransfers2 %>% rename(from = VorigeClub, to = NieuweClub ) %>%  select(from, to) 
 edges = edges %>% group_by(from,to) %>% summarise(weight=n())
 ig = graph_from_data_frame(edges)
 is_weighted(ig)
+
+E(ig)
+edge_attr(ig)
+E(ig)$width <- E(ig)$weight/10
+
+
 plot(
   ig,
   vertex.label.cex=0.7, vertex.size=2, edge.arrow.size=.01 ,
@@ -61,63 +122,69 @@ plot(
   layout = layout_with_graphopt
 )
 
-cut.off <- 1.3*mean(edges$weight) 
-
-net.sp <- delete_edges(ig, E(ig)[weight < 9])
-
-plot(net.sp, vertex.label.cex=0.27, vertex.size=2, edge.arrow.size=.01 ,
-     edge.width	=0.1, edge.arrow.width	 =0.1, edge.color	= "Black",
-     edge.label.cex = 0.4,	
-     layout = layout_with_graphopt) 
-
-
-deg <- degree(ig, mode="all")
 deg = betweenness(ig,directed = FALSE)
-hist(deg)
-
 degreeDF = data.frame(persoon = names(deg), centrality = deg)
 row.names(degreeDF) = NULL
 degreeDF = degreeDF %>% arrange( desc(centrality))
 
-
 plot(
   ig, 
   vertex.size = log(deg+1),
-  vertex.label.cex=0.27,  edge.arrow.size=.01 ,
-  edge.width	=0.1, edge.arrow.width	 =0.1, edge.color	= "Black",
+  vertex.label.cex=0.80,  edge.arrow.size=.1 ,
+  edge.arrow.width	 =0.1, edge.color	= "Black",
   edge.label.cex = 0.1,
   layout = layout_with_graphopt
 )
 
+#############################################################################
+
+nodes = tibble(id = unique(c(edges$from, edges$to))) %>% mutate(label = id)
+
+visNetwork(nodes, edges ) %>% 
+  visEdges(smooth = FALSE) %>% 
+  visOptions(highlightNearest = list(enabled = T, degree = 1, hover = T), nodesIdSelection = TRUE) %>%
+  visIgraphLayout()
 
 
 
+###### Market basket #####################################################
+library(arules)
+AllTransfers2 = 
+  AllTransfers2 %>% 
+  filter(
+    Leeftijd > 16, VorigeClub != "Other", NieuweClub != "Other",
+    NieuweClub != "Einde carrière",
+    NieuweClub != "Zonder club",
+    VorigeClub != "Einde carrière",
+    VorigeClub != "Zonder club"
+    
+    )
 
-ceb <- cluster_edge_betweenness(ig) 
 
-tmp = membership(ceb) 
-NL_communities =  data.frame(
-  persoon = names(tmp), 
-  community = as.numeric(tmp))
+TMP0 = AllTransfers2 %>% mutate(item = cut(Leeftijd,5) %>% as.character()) %>% select(speler, item) %>%  distinct()
+TMP1 = AllTransfers2 %>% select(speler, VorigeClub) %>% rename(item = VorigeClub)
+TMP2 = AllTransfers2 %>% select(speler, NieuweClub) %>% rename(item = NieuweClub)
+TMP3 = AllTransfers2 %>% select(speler, Positie) %>% rename(item = Positie) %>%  distinct()
 
-COMM_STATS = NL_communities %>%  group_by(community) %>%  summarise(n=n())
+MBA = bind_rows( TMP1, TMP2)  %>%  distinct()
 
-dendPlot(ceb, mode ="hclust", use.edge.length = TRUE, cex = 0.1, horiz=TRUE)
-plot(
-  ceb,
-  ig,
-  vertex.label.cex=0.1, vertex.size=1, edge.arrow.size=.01 ,
-  edge.width	=0.1, edge.arrow.width	 =0.1, edge.color	= "Black",
-  edge.label.cex = 0.1,
-  layout = layout_with_graphopt
+transfers = as(
+  split(
+    MBA$item,
+    MBA$speler
+  ),
+  "transactions"
 )
 
+itemFrequencyPlot(transfers, topN = 35)
 
+rules <- apriori(transfers, parameter = list(supp = 0.0005, conf = 0.08, maxlen = 3))
+rules
 
-i = 16
-groep_i = NL_communities %>% filter(community == i) %>% left_join(degreeDF)
-pal <- brewer.pal(9,"BuGn")
-pal <- pal[-(1:4)]
-wordcloud(groep_i$persoon, groep_i$centrality, colors=pal)
+## laat enkele regels zien
+inspect(rules)
+
+inspect( sort(rules, by = "lift")[1:200])
+
 
 
